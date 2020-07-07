2c530cc0d02fc82bb915ba2211eff3ceed002821
Mastermxx <mreijgwart@gmail.com>
2020-07-07 21:47:21 +0200

    almost done

--- js/script.js
+++ js/script.js
@@ -36,17 +36,14 @@ const registerButton = document.querySelector('button');
 // ================  DEFAULT SETTINGS ===============
 // ==================================================
 
-
 // * Default form settings
 nameInput.focus(); // Start in the first input field of the form, user-friendly
 otherTitleInput.style.display = 'none'; // By default hide the input for an other job title
 colorSelection.style.display = 'none'; // By default hide the color options
 
-
 // ==================================================
 // ===================  BASIC INFO ==================
 // ==================================================
-
 
 // If the user selects 'Other' by the 'Job Role' dropdown, show an input field with 'Your Job Role'
 titleDropdown.addEventListener('change', function() {
@@ -54,75 +51,67 @@ titleDropdown.addEventListener('change', function() {
     else otherTitleInput.style.display = 'none';
 });
 
-
-// ==================================================
-// ==================  T-SHIRT INFO =================
-// ==================================================
-/* This is a requiredment for exceed expectations
-1. T Shirt Section
-Hide the "Color" label and select menu until a T-Shirt design is selected from the "Design" menu. */
-
+/* ==================  T-SHIRT INFO =================
+    This is a requirement for exceed expectations
+    1. T Shirt Section - Hide the "Color" label and select menu until a T-Shirt design is selected from the "Design" menu.
+*/
+const colorCircle = document.querySelector('.chosen-color');
 
 designDropdown.addEventListener('change', function() {
 
     const selectedDesign = designDropdown.options[designDropdown.selectedIndex].value;
-    const selectedColor = colorDropdown.options[colorDropdown.selectedIndex].value;
     const colorOptions = colorDropdown.options;
 
     switch(selectedDesign) {
         case 'js puns':
-            colorSelection.style.display = 'block'
-            colorOptions[0].selected = 'true'
-            colorOptions[1].style.display = 'block'
-            colorOptions[2].style.display = 'block'
-            colorOptions[3].style.display = 'block'
-            colorOptions[4].style.display = 'none'
-            colorOptions[5].style.display = 'none'
-            colorOptions[6].style.display = 'none'
+            colorSelection.style.display = 'block';
+            colorOptions[0].selected = true;
+            colorOptions[1].style.display = 'block';
+            colorOptions[2].style.display = 'block';
+            colorOptions[3].style.display = 'block';
+            colorOptions[4].style.display = 'none';
+            colorOptions[5].style.display = 'none';
+            colorOptions[6].style.display = 'none';
+            colorCircle.style.backgroundColor = '#fff';
             break;
         case 'heart js':
-            colorSelection.style.display = 'block'
-            colorOptions[0].selected = 'true'
-            colorOptions[1].style.display = 'none'
-            colorOptions[2].style.display = 'none'
-            colorOptions[3].style.display = 'none'
-            colorOptions[4].style.display = 'block'
-            colorOptions[5].style.display = 'block'
-            colorOptions[6].style.display = 'block'
+            colorSelection.style.display = 'block';
+            colorOptions[0].selected = true;
+            colorOptions[1].style.display = 'none';
+            colorOptions[2].style.display = 'none';
+            colorOptions[3].style.display = 'none';
+            colorOptions[4].style.display = 'block';
+            colorOptions[5].style.display = 'block';
+            colorOptions[6].style.display = 'block';
+            colorCircle.style.backgroundColor = '#fff';
             break;
         default:
-            colorSelection.style.display = 'none'
+            colorSelection.style.display = 'none';
+            colorCircle.style.backgroundColor = '#fff';
     }
-})
+});
 
 
 colorDropdown.addEventListener('change', function() {
-
     const selectedColor = colorDropdown.options[colorDropdown.selectedIndex].value;
-    const colorOptions = colorDropdown.options;
-    const colorCircle = document.querySelector('.chosen-color')
-
-    switch(selectedColor) {
-        case selectedColor:
-            colorCircle.style.backgroundColor = selectedColor;
-            break;
-        case 'Select a color':
-            colorCircle.style.backgroundColor = '#fff';
-            break;
-        default:
-            colorCircle.style.backgroundColor = '#fff';
-    }
-})
-
+    if (selectedColor !== 'default') colorCircle.style.backgroundColor = selectedColor;
+    else colorCircle.style.backgroundColor = '#fff';
+});
 
 // ==================================================
 // ====================  REGISTER ===================
 // ==================================================
 
-
 const checkboxes = Array.from(document.querySelectorAll('input[type=checkbox]'));
+const checkboxArray = [];
+
 let totalCost = 0;
-const checkboxObjects = [];
+
+function updateTotalCost(price, add) {
+    if (add) totalCost = totalCost + parseInt(price);
+    else totalCost = totalCost - parseInt(price);
+    totalCostDiv.textContent = totalCost;
+}
 
 for (let i = 0; i < checkboxes.length; i++) {
     const currentCheckbox = {
@@ -131,132 +120,77 @@ for (let i = 0; i < checkboxes.length; i++) {
         price:   checkboxes[i].getAttribute('data-cost'),
         checked: checkboxes[i].checked,
     };
-    checkboxObjects.push(currentCheckbox);
+    checkboxArray.push(currentCheckbox);
 
     checkboxes[i].addEventListener('change', function(e) {
-        checkboxObjects[i].checked = e.target.checked;
-
-        if (checkboxObjects[i].checked === true) {
-            totalCost = totalCost + parseInt(checkboxObjects[i].price);
-        } else {
-            totalCost = totalCost - parseInt(checkboxObjects[i].price);
-        }
-
-        totalCostDiv.textContent = totalCost;
+    const add = e.target.checked;
+        const price = currentCheckbox.price;
+        updateTotalCost(price, add);
     });
 }
-
 
 // ==================================================
 // ===============  PAYMENT INFO ====================
 // ==================================================
 
-
 paymentDropdown.addEventListener("change", function() {
-
-    switch(paymentDropdown.value) {
-        case 'credit card':
-            creditcardDiv.style.display = 'inherit'
-            paypalDiv.style.display = 'none'
-            bitcoinDiv.style.display = 'none'
-            break;
-        case 'paypal':
-            creditcardDiv.style.display = 'none'
-            paypalDiv.style.display = 'inherit'
-            bitcoinDiv.style.display = 'none'
-            break;
-        case 'bitcoin':
-            creditcardDiv.style.display = 'none'
-            paypalDiv.style.display = 'none'
-            bitcoinDiv.style.display = 'inherit'
-            break;
-        default:
-            creditcardDiv.style.display = 'inherit'
-            paypalDiv.style.display = 'none'
-            bitcoinDiv.style.display = 'none'
-    }
-
+    paypalDiv.style.display = paymentDropdown.value === 'paypal' ? 'inherit' : 'none';
+    bitcoinDiv.style.display = paymentDropdown.value === 'bitcoin' ? 'inherit' : 'none';
+    creditcardDiv.style.display = paymentDropdown.value === 'credit card' ? 'inherit' : 'none';
 });
-
 
 // ==================================================
 // =========  VALIDATION & TOOLTIPS =================
 // ==================================================
 
-
-// Validate name on only letters and spaces.
-function isValidUsername(username) {
-  return /^[a-zA-Z\s]+$/.test(username);
-}
-
+/**
+ * // The Zip Code field should accept a 5-digit number.
+ * @param zipcode - 5-digit number
+ * @returns {boolean}
+*/
+const isValidUsername = (username) => /^[a-zA-Z\s]+$/.test(username);
 // Validate email on (something + @ + something + . + something) example: 'dave@teamtreehouse.com'
-function isValidEmail(email) {
-    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
-}
-
+const isValidEmail = (email) => /^[0-9]{13,16}$/.test(email);
 // Credit Card field should only accept a number between 13 and 16 digits.
-// NEEDS FIX
-function isValidCreditcard(creditcard) {
-    return /^[0-9]{13}$/.test(creditcard);
-}
-
+const isValidCreditcard = (creditcard) => /^[0-9]{13,16}$/.test(creditcard);
 // The Zip Code field should accept a 5-digit number.
-function isValidZipCode(zipcode) {
-    return /^[0-9]{5}$/.test(zipcode);
-}
-
+const isValidZipCode = (zipcode) => /^[0-9]{5}$/.test(zipcode);
 // The CVV should only accept a number that is exactly 3 digits long.
-function isValidCVV(cvv) {
-    return /^[0-9]{3}$/.test(cvv);
-}
+const isValidCVV = (cvv) => /^[0-9]{3}$/.test(cvv);
+
 
 
 function showOrHideTip(show, element) {
   // show element when show is true, hide when false
-  if (show) {
-    element.style.display = 'inherit';
-    element.nextElementSibling.style.borderColor = 'orange'
-  } else {
-    element.style.display = 'none';
-    element.nextElementSibling.style.borderColor = '#4bc970'
-  }
+  element.style.display = show ? 'inherit' : 'none';
+  element.nextElementSibling.style.borderColor = show ? 'orange' : '#4bc970';
 }
 
-const errors = {}
+const errors = {};
 
-function createListener(validator) {
-  return e => {
+function createListener(element, validator) {
+    validate(element, validator);
+    const currentElementObj = element.name.replace('-', '');
+    errors[currentElementObj] = !validator(element.value);
+};
 
-    const inputValue = e.target.value;
-    const valid = validator(inputValue);
-    const showTip = inputValue !== '' && !valid;
-    const tooltip = e.target.previousElementSibling;
-    const currentElement = e.target.name;
-    showOrHideTip(showTip, tooltip);
+const validate = (element, validator) => {
+    const text = element.value;
+    const valid = validator(text);
+    const tooltip = element.previousElementSibling;
+    showOrHideTip(!valid, tooltip);
+};
 
-    const currentElementObj = e.target.name.replace('-', '');
-    if(valid) errors[currentElementObj] = true;
-    else errors[currentElementObj] = false;
-    console.log(currentElement)
-  };
-}
-
-
-// Validation Basic Info
-nameInput.addEventListener('input', createListener(isValidUsername));
-emailInput.addEventListener('input', createListener(isValidEmail));
-// Validation Payment Info
-ccInput.addEventListener('input', createListener(isValidCreditcard));
-zipInput.addEventListener('input', createListener(isValidZipCode));
-cvvInput.addEventListener('input', createListener(isValidCVV));
+nameInput.addEventListener('input', () => createListener(nameInput, isValidUsername));
+emailInput.addEventListener('input', () => createListener(emailInput, isValidEmail));
+ccInput.addEventListener('input', () => createListener(ccInput, isValidCreditcard));
+zipInput.addEventListener('input', () => createListener(zipInput, isValidZipCode));
+cvvInput.addEventListener('input', () => createListener(cvvInput, isValidCVV));
 
 
 // ==================================================
 // =========  VALIDATION ON SUBMIT =================
 // ==================================================
-
-
-// registerButton
 
 // Check if name is not null & valid
 // check if email is not null & valid
@@ -267,43 +201,27 @@ cvvInput.addEventListener('input', createListener(isValidCVV));
 // a 5-digit zip code,
 // and 3-number CVV value.
 
-// if all this is the case, you are able to submit
-
-
-document.body.addEventListener('click', function() {
-    console.log(errors)
-
-    if( isValidUsername(nameInput.value) &&
-        isValidEmail(emailInput.value) &&
-        isValidCreditcard(ccInput.value) &&
-        isValidZipCode(zipInput.value) &&
-        isValidCVV(cvvInput.value)) {
-            registerButton.disabled = false;
-        }
-});
-//
-//
-registerButton.addEventListener('click', function() {
+registerButton.addEventListener('click', function(event) {
     event.preventDefault();
+
+
+
     const allInputs = [
         {element: nameInput, validator: isValidUsername},
         {element: emailInput, validator: isValidEmail},
-        {element: ccInput, validator: isValidCreditcard},
-        {element: zipInput, validator: isValidZipCode},
-        {element: cvvInput, validator: isValidCVV},
+        {element: ccInput, validator: isValidCreditcard}, //2
+        {element: zipInput, validator: isValidZipCode}, // 3
+        {element: cvvInput, validator: isValidCVV}, // 4
     ];
 
-    const validate = (el, validator) => {
-        const text = el.value;
-        const valid = validator(text);
-        const tooltip = el.previousElementSibling;
-        showOrHideTip(!valid, tooltip);
-    };
-
-    for(let i = 0; i < allInputs.length; i++){
+    for (let i = 0; i < allInputs.length; i++) {
+        // if (i > 1 && paymentDropdown.value !== 'credit card') 
         const currentInput = allInputs[i];
-        const inputIsValid = currentInput.validator(currentInput.element.value)
-        if(!inputIsValid){
+        const currentValue = currentInput.element.value; // input value
+        const currentValidator = currentInput.validator; // regex validator
+        const inputIsValid = currentValidator(currentValue); // validate
+
+        if (!inputIsValid) {
             allInputs.forEach( input => {
                 validate(input.element, input.validator);
             });
@@ -313,4 +231,4 @@ registerButton.addEventListener('click', function() {
 
     /// submit form
     console.log('All is OK!');
-})
+});
